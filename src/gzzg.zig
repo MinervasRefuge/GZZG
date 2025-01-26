6ef6b53d9731f17d29bed3a6dfd584d48dc1f3af
Abby <minervasrefuge@noreply.codeberg.org>
2025-01-27 07:00:38 +1030

    Finalise Boolean with fmt corrections

--- src/gzzg.zig
+++ src/gzzg.zig
@@ -270,22 +270,20 @@ pub const ForeignType = SCMWrapper(null);
 pub const Boolean = struct {
     s: guile.SCM,
 
+    // zig fmt: off
+    pub const TRUE : Boolean = .{ .s = guile.SCM_BOOL_T };
+    pub const FALSE: Boolean = .{ .s = guile.SCM_BOOL_F };
+
     //const BooleanTrait = struct {
-    pub fn from(b: bool) Boolean {
-        return .{ .s = if (b) guile.SCM_BOOL_T else guile.SCM_BOOL_F };
-    }
+    pub fn from(b: bool) Boolean { return if (b) TRUE else FALSE; }
 
-    pub fn toZ(a: Boolean) bool {
-        return guile.scm_to_bool(a.s) != 0;
-    }
+    pub fn toZ(a: Boolean) bool { return guile.scm_to_bool(a.s) != 0; }
 
-    pub fn is(a: Boolean) Boolean {
-        return .{ .s = guile.scm_boolean_p(a.s) };
-    }
+    pub fn is (a: Boolean) Boolean { return .{ .s = guile.scm_boolean_p(a.s) }; }
+    pub fn isZ(a: Boolean) bool    { return guile.scm_is_bool(a.s) != 0; }
 
-    pub fn isZ(a: Boolean) bool {
-        return guile.scm_is_bool(a.s) != 0;
-    }
+    pub fn not(a: Boolean) Boolean { return .{ .s = guile.scm_not(a.s) }; }
+    // zig fmt: on
 };
 
 //                                          -------------
