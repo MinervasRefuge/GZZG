a8635865e580bfdb201a3c32efeaeb90c2ba3b75
Abby <minervasrefuge@noreply.codeberg.org>
2025-03-26 11:07:05 +1030

    Further Port fns add

--- src/port.zig
+++ src/port.zig
@@ -12,6 +12,8 @@ const Any        = gzzg.Any;
 const Boolean    = gzzg.Boolean;
 const ByteVector = gzzg.ByteVector;
 const Number     = gzzg.Number;
+const String     = gzzg.String;
+const Symbol     = gzzg.Symbol;
 
 //                                          ------------
 //                                          Port §6.12.1
@@ -85,13 +87,44 @@ pub const Port = struct {
         _ = guile.scm_put_bytevector(a.s, bv.s, orUndefined(start), orUndefined(count));
     }
         
-    // * TODO §6.12.3 Encoding                             :incomplete:allFunctions:
-    //
+    // * DONE §6.12.3 Encoding                             :complete:allFunctions:
+    // 
 
-    // scm_port_encoding (port)
-    // scm_set_port_encoding_x (port, enc)
-    // scm_port_conversion_strategy (port)
-    // scm_set_port_conversion_strategy_x (port, sym)
+    pub fn encoding   (a: Port)              String { return .{ .s = guile.scm_port_encoding(a.s) }; }
+    pub fn setEncoding(a: Port, enc: String) void   { _ = guile.scm_set_port_encodingx_x(a.s, enc.s); }
+    
+    pub const ConversionStrategy = enum {
+        const cache = gzzg.StaticCache(Symbol, std.meta.fieldNames(@This()));
+        
+        @"error",
+        substitute,
+        escape,
+
+        pub fn get(a: @This()) Symbol {
+            return switch(a) {
+                inline else => |t| cache.get(@tagName(t))
+            };
+        }
+
+        pub fn to(a: Symbol) ?@This() {
+            inline for(std.meta.fields(@This())) |field| {
+                if (gzzg.eqZ(cache.get(field.name), a))
+                    return @enumFromInt(field.value);
+            }
+
+            return null;
+        }
+    };
+
+    pub fn conversionStrategy(a: ?Port) ConversionStrategy { 
+        const p = if (a != null) a.s else Boolean.FALSE.s;
+        return .to(.{ .s = guile.scm_port_conversion_strategy(p) }).?; 
+    }
+
+    pub fn setConversionStrategy(a: ?Port, cs: ConversionStrategy) void {
+        const p = if (a != null) a.s else Boolean.FALSE.s;
+        guile.scm_set_port_conversion_strategy(p, cs.get());
+    }
 
     // * TODO §6.12.4 Textual I/O                          :incomplete:allFunctions:
     //
@@ -101,50 +134,95 @@ pub const Port = struct {
     // scm_set_port_column_x (port, column)
     // scm_set_port_line_x (port, line)
     
-    // * TODO §6.12.6 Buffering                            :incomplete:allFunctions:
-    //
+    // * DONE §6.12.6 Buffering                            :complete:allFunctions:
+    // 
 
-    // scm_setvbuf (port, mode, size)
-    // scm_force_output (port)
-    // scm_flush_all_ports ()
-    // scm_drain_input (port)
+    pub const BufferingMode = union(enum) {
+        const cache = gzzg.StaticCache(Symbol, std.meta.fieldNames(@This()));
+        
+        none,
+        line,
+        block:?Number,
 
-    // * TODO §6.12.7 Random Access                        :incomplete:allFunctions:
-    //
+        pub fn get(a: @This()) Symbol {
+            return switch(a) {
+                inline else => |t| cache.get(@tagName(t))
+            };
+        }
+    };
 
-    // scm_seek (fd_port, offset, whence)
-    // scm_ftell (fd_port)
-    // scm_truncate_file (file, length)
+    pub fn setvbuf(a: Port, mode: BufferingMode) void {
+        const size = if (mode == .block) mode.block else null;
+        guile.scm_setvbuf(a.s, mode.get().s, orUndefined(size));
+    }
 
-    // * TODO §6.12.9 Default Ports                       :incomplete:allFunctions:
+    pub fn forceOutput  (a: Port) void { _ = guile.scm_force_output(a.s); }
+    pub fn flushAllPorts()        void { _ = guile.scm_flush_all_ports(); }
+    pub fn drainInput   (a: Port) String { return .{ .s = guile.scm_drain_input(a.s) }; }
+
+    // * DONE §6.12.7 Random Access                        :complete:allFunctions:
+    // 
+
+    pub const Whence = enum(u2) {
+        // Guile uses the same values are ~fseek~
+        const S = std.os.linux.SEEK;
+        
+        set     = S.SET,
+        current = S.CUR,
+        end     = S.END,
+
+        // Integer
+        pub fn toNumber(a: @This()) Number {
+            return .from(@intFromEnum(a));
+        }
+    };
+
+    //todo Integer
+    pub fn seek(a: Port, offset: Number, whence: Whence) Number { return .{ .s = guile.scm_seek(a.s, offset.s, whence.toNumber()) }; }
+    pub fn ftell(a: Port) Number { return .{ .s = guile.scm_ftell(a.s) }; }
+    pub fn truncateFile(a: Port, length: ?Number) void { guile.scm_truncate_file(a.s, orUndefined(length)); }
+
+    // * DONE §6.12.9 Default Ports                          :complete:allFunctions:
+    // 
 
     pub fn currentInput () Port { return .{ .s = guile.scm_current_input_port () }; }
     pub fn currentOutput() Port { return .{ .s = guile.scm_current_output_port() }; }
     pub fn currentError () Port { return .{ .s = guile.scm_current_error_port () }; }
 
-    // scm_set_current_input_port (port)
-    // scm_set_current_output_port (port)
-    // scm_set_current_error_port (port)
+    pub fn setCurrentInput (a: Port) void { _ = guile.scm_set_current_input_port (a.s); }
+    pub fn setCurrentOutput(a: Port) void { _ = guile.scm_set_current_output_port(a.s); }
+    pub fn setCurrentError (a: Port) void { _ = guile.scm_set_current_error_port (a.s); }
 
-    // void scm_dynwind_current_input_port (SCM port)
-    // void scm_dynwind_current_output_port (SCM port)
-    // void scm_dynwind_current_error_port (SCM port)
+    pub fn dynwindCurrentInput (a: Port) void { guile.scm_dynwind_current_input_port (a.s); }
+    pub fn dynwindCurrentOutput(a: Port) void { guile.scm_dynwind_current_output_port(a.s); }
+    pub fn dynwindCurrentError (a: Port) void { guile.scm_dynwind_current_error_port (a.s); }
 
     // * TODO §6.12.10 Type of Ports                                    :incomplete:
     // 
 
-    // * TODO §6.12.12 Using Ports from C                  :incomplete:allFunctions:
-    //
+    // * DONE §6.12.12 Using Ports from C                  :complete:allFunctions:
+    // 
 
-    // size_t scm_c_read (SCM port, void *buffer, size_t size)
-    // void scm_c_write (SCM port, const void *buffer, size_t size)
-    // size_t scm_c_read_bytes (SCM port, SCM bv, size_t start, size_t count)
-    // void scm_c_write_bytes (SCM port, SCM bv, size_t start, size_t count)
-    // void scm_unget_bytes (const unsigned char *buf, size_t len, SCM port)
-    // void scm_unget_byte (int c, SCM port)
-    // void scm_ungetc (scm_t_wchar c, SCM port)
-    // void scm_c_put_latin1_chars (SCM port, const scm_t_uint8 *buf, size_t len)
-    // void scm_c_put_utf32_chars (SCM port, const scm_t_uint32 *buf, size_t len)
+    pub fn readZ (a: Port, buffer: []u8)       usize { return guile.scm_c_read (a.s, buffer.ptr, buffer.len); }
+    pub fn writeZ(a: Port, buffer: []const u8) void  {        guile.scm_c_write(a.s, buffer.ptr, buffer.len); }
+
+    pub fn readBytesZ(a: Port, bv: ByteVector, start: usize, count: usize) usize
+        { return guile.scm_c_read_bytes(a.s, bv.s, start, count); }
+    
+    pub fn writeBytesZ(a: Port, bv: ByteVector, start: usize, count: usize) void
+        { guile.scm_c_write_bytes(a.s, bv.s, start, count); }
+
+    pub fn ungetBytesZ(a: Port, buffer:[]u8) void { guile.scm_unget_bytes(buffer.ptr, buffer.len, a.s); }
+    pub fn ungetByteZ (a: Port, byte: u8)    void { guile.scm_unget_byte(byte, a.s); } 
+    
+    // todo: is it i32/u32 or u21?
+    pub fn ungetC(a: Port, c: i32) void { guile.scm_ungetc(c, a.s); }
+    pub fn putLatin1(a: Port, buffer: []const u8)  void { guile.scm_c_put_latin1_chars(a.s, buffer.ptr, buffer.len); }
+    pub fn putUTF32 (a: Port, buffer: []const u32) void { guile.scm_c_put_utf32_chars (a.s, buffer.ptr, buffer.len); }
+
+    // Extra?
+
+    pub fn flush(a: Port) void { guile.scm_flush(a.s); }
     
     comptime {
         _ = gzzg.contracts.GZZGType(@This(), void);
