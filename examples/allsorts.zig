2dcad6bb9b4447e72a5f70f489fb01317b6e32d9
Abby <minervasrefuge@noreply.codeberg.org>
2025-04-28 12:53:37 +0930

    Relicense Examples under GPL3+

--- examples/allsorts.zig
+++ examples/allsorts.zig
@@ -1,4 +1,19 @@
-// BSD-3-Clause : Copyright © 2025 Abigale Raeck.
+// allsorts.zig : Provides a basic example of GZZG usage.
+// Copyright (C) 2025  Abigale Raeck
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // zig fmt: off
 
 const std   = @import("std");
